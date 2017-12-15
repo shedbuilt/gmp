492d58536581796da857e2233e2077a453769c17
Auston Stewart <auston@shedbuilt.net>
2017-12-15 10:31:52 -1000

    Initial commit of gmp

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./configure --prefix=/usr    \
+            --enable-cxx     \
+            --disable-static \
+            --docdir=/usr/share/doc/gmp-6.1.2
+make -j $SHED_NUMJOBS
+make html
+make DESTDIR=$SHED_FAKEROOT install
+make DESTDIR=$SHED_FAKEROOT install-html
