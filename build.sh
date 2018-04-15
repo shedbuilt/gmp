984801d8aa69969dbf44fb771c78d7376b7dc655
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:23:48 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
             --enable-cxx     \
             --disable-static \
             --docdir=/usr/share/doc/gmp-6.1.2
-make -j $SHED_NUMJOBS
+make -j $SHED_NUM_JOBS
 make html
-make "DESTDIR=$SHED_FAKEROOT" install
-make "DESTDIR=$SHED_FAKEROOT" install-html
+make "DESTDIR=$SHED_FAKE_ROOT" install
+make "DESTDIR=$SHED_FAKE_ROOT" install-html
