31048915a6359719ee62c0727ebb97b9372cd8a1
Auston Stewart <auston@shedbuilt.net>
2018-05-29 18:15:00 -1000

    Adding build options

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VESION}"
 ./configure --prefix=/usr    \
             --enable-cxx     \
             --disable-static \
-            --docdir=/usr/share/doc/gmp-6.1.2
-make -j $SHED_NUM_JOBS
-make html
-make "DESTDIR=$SHED_FAKE_ROOT" install
-make "DESTDIR=$SHED_FAKE_ROOT" install-html
+            --docdir=${SHED_PKG_LOCAL_DOCDIR} &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Install Docs
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    make html &&
+    make DESTDIR="$SHED_FAKE_ROOT" install-html || exit 1
+else
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
